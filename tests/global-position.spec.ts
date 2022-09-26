224981dc75916e4e2aa9bd2edb3a6aafea293ce0
michalusio <micha_i@interia.pl>
2022-09-26 23:42:41 +0200

    Added additional GP test

--- tests/global-position.spec.ts
+++ tests/global-position.spec.ts
@@ -9,6 +9,7 @@ describe("GlobalPosition", () => {
   describe("fromRoomPosition", () => {
     test.concurrent.each([
       [new RoomPosition(12, 12, "E2S3"), new GlobalPosition(112, 162)],
+      [new RoomPosition(49, 49, "W0N0"), new GlobalPosition(-1, -1)],
       [new RoomPosition(0, 0, "E0S0"), new GlobalPosition(0, 0)],
       [new RoomPosition(20, 20, "W20S1"), new GlobalPosition(-1030, 70)],
       [new RoomPosition(10, 49, "W3N5"), new GlobalPosition(-190, -251)]
@@ -22,6 +23,7 @@ describe("GlobalPosition", () => {
     test.concurrent.each([
       [new GlobalPosition(112, 162), new RoomPosition(12, 12, "E2S3")],
       [new GlobalPosition(0, 0), new RoomPosition(0, 0, "E0S0")],
+      [new GlobalPosition(-1, -1), new RoomPosition(49, 49, "W0N0")],
       [new GlobalPosition(-1030, 70), new RoomPosition(20, 20, "W20S1")],
       [new GlobalPosition(-190, -251), new RoomPosition(10, 49, "W3N5")]
     ])("for globalPos %O should return %O", async (globalPos, expected) => {
