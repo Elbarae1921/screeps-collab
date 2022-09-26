6121c41a6f9a2546b26c15468817bd3ac3ac5596
michalusio <micha_i@interia.pl>
2022-09-27 01:23:38 +0200

    Added time caching for Property

--- src/utils/property.ts
+++ src/utils/property.ts
@@ -3,13 +3,19 @@ import { Reference } from "./reference";
 export class Property<A extends _HasId, T> {
   constructor(private instance: Reference<A>, private map: (instance: A) => T) {}
 
+  private time?: number;
+  private _value?: T;
+
   public get value(): T | null {
     const instance = this.instance.get;
     if (!instance) return null;
-    return this.map(instance);
+    if (!this.time || this.time !== Game.time) {
+      this._value = this.map(instance);
+      this.time = Game.time;
+    }
+    return this._value!;
   }
 }
-
 
 export class ConstantProperty<A extends _HasId, T> extends Property<A, T> {
   constructor(instance: Reference<A>, map: (instance: A) => T) {
